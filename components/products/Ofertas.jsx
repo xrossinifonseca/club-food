b5f7e9bec2e190b2781956cc5854d36cb81d908c
Rossini Fonseca <rossinidrum@outlook.com>
2022-09-12 19:22:09 -0300

    tratamento de erros

--- components/products/Ofertas.jsx
+++ components/products/Ofertas.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useStateContext } from "../../context/StateContext";
 import { urlFor } from "../../lib/client";
 
-const Ofertas = ({ ofertas: { image, details, price, name } }) => {
+const Ofertas = ({ ofertas }) => {
+  const { image, details, price, name } = ofertas;
+  const { addToCart, qty } = useStateContext();
+
   return (
     <div className="product-oferta">
       <img src={urlFor(image && image[0])} className="oferta-image" />
       <p>{name}</p>
       <p className="oferta-detail">{details}</p>
       <h5>R$ {price}/unidade</h5>
-      <button type="button" className="btn-cart">
+      <button
+        type="button"
+        className="btn-cart"
+        onClick={() => addToCart(ofertas, qty)}
+      >
         Adicionar
       </button>
     </div>
